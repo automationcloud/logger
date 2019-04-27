a8b87e5c43154c9ef4b2a1744eb3806391cb058b
Anatoliy <mail@anatoliy.in>
2019-04-27 16:24:29 +0100

    Make mock props public

--- mock/transport.go
+++ mock/transport.go
@@ -3,14 +3,14 @@ package mock
 import "github.com/automationcloud/logging"
 
 type Transport struct {
-	errorEntry *logging.ErrorEntry
-	logEntry   *logging.LogEntry
+	ErrorEntry *logging.ErrorEntry
+	LogEntry   *logging.LogEntry
 }
 
 func (ft *Transport) SendLog(l *logging.LogEntry) {
-	ft.logEntry = l
+	ft.LogEntry = l
 }
 
 func (ft *Transport) ReportError(l *logging.ErrorEntry) {
-	ft.errorEntry = l
+	ft.ErrorEntry = l
 }
